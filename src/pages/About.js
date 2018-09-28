a2891a92d3ba4e7ff446c6cda026acef20308d1c
Craveburn <raburncameron@gmail.com>
2018-09-27 22:22:29 -0600

    finished skeleton of about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import './About.css'
 import { SocialIcon } from 'react-social-icons'
-import PicoBoys from '../pics/Dad-bod-boys.jpg'
+
+import Lucas from '../pics/LucasOrton.jpg'
+import Marcus from '../pics/MarcusMarinos.jpg'
+import Matt from '../pics/MatthewMarinos.jpg'
+import Mike from '../pics/MichaelMarinos.jpg'
 
 import Navbar from '../navigation/Navbar.js'
 
@@ -12,7 +16,16 @@ export default class extends Component {
                 <Navbar />
                 <div className="aboutBody">
                     <h1>Dad bod is a band.</h1>
-                    <div className="aboutImage" style={{ backgroundImage: PicoBoys }}></div>
+                    <div className="theBoys">
+                        <div className="mikey" style={{ backgroundImage: Mike }}></div>
+                        <h3>Michael Marinos: Guitar, Vocals</h3>
+                        <div className="lucas" style={{ backgroundImage: Lucas }}></div>
+                        <h3>Lucas Orton: Synth, Vocals</h3>
+                        <div className="marcus" style={{ backgroundImage: Marcus }}></div>
+                        <h3>Marcus Marinos: Drums, Vocals, Shaker egg</h3>
+                        <div className="matt" style={{ backgroundImage: Matt }}></div>
+                        <h3>Matthew Marinos: Bass, Vocals</h3>
+                    </div>
                     <div className="social">
                         <SocialIcon network="instagram" url="https://www.instagram.com/officialdadbod/" />
                         <SocialIcon network="facebook" url="https://www.facebook.com/officialdadbod/" />
