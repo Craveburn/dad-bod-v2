379d7d81ea4dbc2003b0f2114cd412d56b299c0c
Craveburn <raburncameron@gmail.com>
2018-09-27 22:23:17 -0600

    added minor css changes and put band pic on main page.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
+import './Home.css'
+
+import TheBoys from '../pics/Dad-bod-boys.jpg'
+
 import Navbar from '../navigation/Navbar.js'
 
 export default class extends Component {
     render() {
         return (
-            <div>
+            <div className='containter'>
                 <Navbar />
-
-                this is the mothafuckin home page
+                <div className='homeBody'>
+                    <div className="aboutImage" style={{ backgroundImage: TheBoys }}></div>
+                    this is the mothafuckin home page
                 </div>
+            </div>
         )
     }
 }
