e5c9ee64df495e82f9132ba3ec24d80ec65c96ff
Craveburn <raburncameron@gmail.com>
2018-10-10 23:55:07 -0600

    work on flexbox on this part, items need to be columnized

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import './Home.css'
 
+import Navbar from '../navigation/Navbar.js'
+
 import TheBoys from '../pics/Dad-bod-boys.jpg'
 
-import Navbar from '../navigation/Navbar.js'
 
 export default class extends Component {
     render() {
         return (
-            <div className='containter'>
+            <div className='container'>
                 <Navbar />
                 <div className='homeBody'>
-                    <div className="aboutImage" style={{ backgroundImage: TheBoys }}></div>
-                    this is the mothafuckin home page
+                    <span><div className="aboutImage" style={{ backgroundImage: TheBoys }}></div></span>
+                    <span><p>this is the mothafuckin home page</p></span>
                 </div>
             </div>
         )
