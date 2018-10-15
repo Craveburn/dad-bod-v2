b3c5c53e0373c0f31f23c6c928dce2578900a9cc
Craveburn <raburncameron@gmail.com>
2018-10-15 00:06:58 -0600

    added a spotify player to the page, need to use functions to style it better.

--- src/pages/Music.js
+++ src/pages/Music.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 import Navbar from '../navigation/Navbar.js'
 
+import SpotifyPlayer from 'react-spotify-player'
+
+import './Music.css'
+
 export default class extends Component {
+
+    mediaPlayer = () => {
+        var size = {
+            width: '100%',
+            height: 200,
+        };
+        var view = 'coverart'
+        var theme = 'black'
+    }
+
     render() {
         return (
-            <div>
+            <div className="musicContainer">
                 <Navbar />
-                THIS IS WHERE THE MUSIC LINKS AND SHIT GOES BOYO
+                <div className="musicBody">
+                    <div>
+                        <SpotifyPlayer
+                        uri="spotify:track:3FSXtfCfI4rdAy1VtwRgN9"
+                        size={this.mediaPlayer.size}
+                        view={this.mediaPlayer.view}
+                        theme={this.mediaPlayer.theme}
+                        />
+                    </div>
+                </div>
             </div>
         )
     }
