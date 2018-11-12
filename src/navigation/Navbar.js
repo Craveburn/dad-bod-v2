542146af28198f81c34e183a22cf6abc2f6f82a0
Craveburn <raburncameron@gmail.com>
2018-11-11 22:24:43 -0700

    added more defining divs to social vs page/logos. to help with css.

--- src/navigation/Navbar.js
+++ src/navigation/Navbar.js
@@ -12,13 +12,17 @@ export default class extends Component {
             <div className='wrapper'>
                 <div className="logoContainer">
                 <ul>
+                <div className="pageLinks">
                 <li><Link to="/" className="logoLink"><span><div className="logo" style={{ backgroundImage: Logo }}></div></span></Link></li>
                 <li><Link to="/about" className="page"><span>About</span></Link></li>
                 <li><Link to="/music" className="page"><span>Music</span></Link></li>
                 <li><Link to="/shows" className="page"><span>Shows</span></Link></li>
                 <li><Link to="/merch" className="page"><span>Merch</span></Link></li>
+                </div>
+                <div className="socialLinks">
                 <li><a href="https://www.facebook.com/officialdadbod/" className="page" target="_blank" rel="noopener noreferrer"><span><Icon name='facebook f'></Icon></span></a></li>
                 <li><a href="https://www.instagram.com/officialdadbod/" className="page" target="_blank" rel="noopener noreferrer"><span><Icon name='instagram'></Icon></span></a></li>
+                </div>
                 </ul>
                 </div>
             </div>
