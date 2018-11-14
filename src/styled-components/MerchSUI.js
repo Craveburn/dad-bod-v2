7b61cb745bd603f0a5741663841015293b8cd5f0
Craveburn <raburncameron@gmail.com>
2018-11-13 21:14:20 -0700

    finally got the navbar working correctly.

--- src/styled-components/MerchSUI.js
+++ src/styled-components/MerchSUI.js
@@ -1,31 +1,32 @@
 import React from 'react'
 import { Grid, Image } from 'semantic-ui-react'
 
-import YellowShirt from '../pics/DadBod-yellowShirt.jpg'
+// import YellowShirt from '../pics/DadBod-yellowShirt.jpg'
+import BlackDadBodHat from '../pics/BlackDadBodHat.jpg'
 
 const GridExampleColumns = () => (
   <Grid>
     <Grid.Row columns={3}>
       <Grid.Column width={5}>
-        <Image src={YellowShirt} />
+        <Image src={BlackDadBodHat} />
       </Grid.Column>
       <Grid.Column width={5}>
-        <Image src={YellowShirt} />
+        <Image src={BlackDadBodHat} />
       </Grid.Column>
       <Grid.Column width={5}>
-        <Image src={YellowShirt} />
+        <Image src={BlackDadBodHat} />
       </Grid.Column>
     </Grid.Row>
 
     <Grid.Row columns={3}>
       <Grid.Column width={5}>
-        <Image src={YellowShirt} />
+        <Image src={BlackDadBodHat} />
       </Grid.Column>
       <Grid.Column width={5}>
-        <Image src={YellowShirt} />
+        <Image src={BlackDadBodHat} />
       </Grid.Column>
       <Grid.Column width={5}>
-        <Image src={YellowShirt} />
+        <Image src={BlackDadBodHat} />
       </Grid.Column>
     </Grid.Row>
   </Grid>
