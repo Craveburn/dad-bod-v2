366c134504436098d0fb945e89c3c85bdb129e97
Craveburn <raburncameron@gmail.com>
2019-05-28 14:35:11 -0600

    what the fuck is wrong with me.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,20 +5,63 @@ import "../CSS/Home.css";
 
 import Navbar from "../navigation/Navbar.js";
 
-import TheBoys from "../pics/UpdatedBandPhoto.jpg";
-
 export default class extends Component {
   render() {
     return (
       <div className="container">
         <Navbar />
+
+        <div className="playerWrapper">
+          <ReactPlayer
+            className="videoPlayer"
+            url="https://youtu.be/-4CR3aGC8aQ"
+            width="100%"
+            height="100%"
+            controls
+          />
+        </div>
         <div className="homeBody">
-          <div className="videoPlayer">
-            <ReactPlayer url="https://youtu.be/-4CR3aGC8aQ" playing />
-          </div>
+          <div className="homeText">
           <p>Dad Bod - "Don't Say"</p>
-          {/* <img src={TheBoys} alt="pic of the boys in the band" />
-          <p>this is the mothafuckin home page</p> */}
+          <p>
+            Lyrics:
+            <br />
+            Don't Say
+            <br />
+            You're gonna be there anyway
+            <br />
+            Don't Say, Don't Say
+            <br />
+            Don't Say
+            <br />
+            You're gonna leave me anyway
+            <br />
+            Don't Say, Don't Say
+            <br />
+            These days
+            <br />
+            Feel like I'm sinking down below
+            <br />
+            These days, won't stay
+            <br />
+            You say
+            <br />
+            You've got nothing on your mind
+            <br />
+            You say, don't say
+            <br />
+            Where did it go
+            <br />
+            Back in time? Let me know
+            <br />
+            I'll let you show
+            <br />
+            All the things, that make it so
+            <br />
+            We'd like to dedicate this video to Stephen Fitzpatrick and Audun
+            Laading of "Her's".
+          </p>
+          </div>
         </div>
       </div>
     );
